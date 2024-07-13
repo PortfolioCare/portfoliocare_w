0206ad40889683dc8f060befaeda33ed4ae96220
Aiden-Coding <dwx_job@163.com>
2024-07-13 16:07:41 +0800

    1

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 This template should help get you started developing with Vue 3 and TypeScript in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
 
 Learn more about the recommended Project Setup and IDE Support in the [Vue Docs TypeScript Guide](https://vuejs.org/guide/typescript/overview.html#project-setup).
+
+## env
+
+请注意，Vite 只会注入以 VITE_ 前缀开头的环境变量。这是为了防止无意中暴露敏感信息。因此，请确保你的环境变量名称以 VITE_ 开头。
--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,46 @@
 import path from "node:path";
 import vue from "@vitejs/plugin-vue";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 import tailwind from "tailwindcss";
 import autoprefixer from "autoprefixer";
 import electron from "vite-plugin-electron/simple";
-export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwind(), autoprefixer()],
-    },
-  },
-  plugins: [
-    vue(),
-    electron({
-      main: {
-        // Shortcut of `build.lib.entry`.
-        entry: path.resolve(__dirname, "electron/main.ts"),
+export default defineConfig(({ mode }) => {
+  // 加载环境变量
+  const env = loadEnv(mode, process.cwd());
+  console.log(env);
+  return {
+    css: {
+      postcss: {
+        plugins: [tailwind(), autoprefixer()],
       },
-      preload: {
-        // Shortcut of `build.rollupOptions.input`.
-        // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
-        input: path.resolve(__dirname, "electron/preload.ts"),
-      },
-      // Ployfill the Electron and Node.js API for Renderer process.
-      // If you want use Node.js in Renderer process, the `nodeIntegration` needs to be enabled in the Main process.
-      // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
-      renderer:
-        process.env.NODE_ENV === "test"
-          ? // https://github.com/electron-vite/vite-plugin-electron-renderer/issues/78#issuecomment-2053600808
-            undefined
-          : {},
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
     },
-  },
+    plugins: [
+      vue(),
+      electron({
+        main: {
+          // Shortcut of `build.lib.entry`.
+          entry: path.resolve(__dirname, "electron/main.ts"),
+        },
+        preload: {
+          // Shortcut of `build.rollupOptions.input`.
+          // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
+          input: path.resolve(__dirname, "electron/preload.ts"),
+        },
+        // Ployfill the Electron and Node.js API for Renderer process.
+        // If you want use Node.js in Renderer process, the `nodeIntegration` needs to be enabled in the Main process.
+        // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
+        renderer:
+          process.env.NODE_ENV === "test"
+            ? // https://github.com/electron-vite/vite-plugin-electron-renderer/issues/78#issuecomment-2053600808
+              undefined
+            : {},
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
 });
