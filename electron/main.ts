bb0bbff23cc6fc694328cd7d1692f4881f56a206
Aiden-Coding <dwx_job@163.com>
2024-07-27 11:07:46 +0800

    1

--- electron/main.ts
+++ electron/main.ts
@@ -65,80 +65,29 @@ function createWindow() {
     // win.loadFile('dist/index.html')
     win.loadFile(path.join(RENDERER_DIST, "index.html"));
   }
-  /** 窗口移动功能封装 */
-  // 窗口移动 位置刷新定时器
-  let movingInterval: NodeJS.Timeout | null;
-  /**
-   * 窗口移动事件
-   */
-  ipcMain.on("window-move-open", (event, canMoving) => {
-    // 获取当前鼠标聚焦的窗口
-    const currentWindow = BrowserWindow.getFocusedWindow();
 
-    if (currentWindow) {
-      const currentWindowSize = currentWindow.getSize();
-      if (canMoving) {
-        // 读取原位置
-        const winPosition = currentWindow.getPosition();
-        let winStartPosition = screen.getCursorScreenPoint();
-        // 清除旧的定时器
-        if (movingInterval) {
-          clearInterval(movingInterval);
-        }
-        // 创建定时器，每10毫秒更新一次窗口位置，保证一致
-        movingInterval = setInterval(() => {
-          // 窗口销毁判断，高频率的更新有可能窗口已销毁，定时器还没结束，此时就会出现执行销毁窗口方法的错误
-          if (!currentWindow.isDestroyed()) {
-            // 如果窗口失去焦点，则停止移动
-            if (!currentWindow.isFocused()) {
-              if (movingInterval) clearInterval(movingInterval);
-              movingInterval = null;
-            }
-            // 实时更新位置
-            const cursorPosition = screen.getCursorScreenPoint();
-            const x = cursorPosition.x - winStartPosition.x + winPosition[0];
-            const y = cursorPosition.y - winStartPosition.y + winPosition[1];
-            // 更新位置的同时设置窗口原大小， windows上设置=>显示设置=>文本缩放 不是100%时，窗口会拖拽放大
-            currentWindow.setBounds({
-              x: x,
-              y: y,
-              width: currentWindowSize[0],
-              height: currentWindowSize[1],
-            });
-          } else {
-            if (movingInterval) clearInterval(movingInterval);
-            movingInterval = null;
-          }
-        }, 10);
-      } else {
-        if (movingInterval) clearInterval(movingInterval);
-        movingInterval = null;
-      }
-    }
-  });
-  workerProcess = child_process.exec(
-    "start /b C:\\Users\\Administrator\\.jdks\\openjdk-21.0.1\\bin\\java.exe -jar trading-life-java.jar",
-    function (error, stdout, stderr) {
-      if (error) {
-        console.log("error");
-        console.log(error.stack);
-        console.log("Error code: " + error.code);
-        console.log("Signal received: " + error.signal);
-      }
+  // workerProcess = child_process.exec(
+  //   "start /b C:\\Users\\Administrator\\.jdks\\openjdk-21.0.1\\bin\\java.exe -jar trading-life-java.jar",
+  //   function (error, stdout, stderr) {
+  //     if (error) {
+  //       console.log("error");
+  //       console.log(error.stack);
+  //       console.log("Error code: " + error.code);
+  //       console.log("Signal received: " + error.signal);
+  //     }
 
-      console.log("stdout");
-      console.log("stdout: " + stdout);
-      console.log("stderr: " + stderr);
-    }
-  );
+  //     console.log("stdout");
+  //     console.log("stdout: " + stdout);
+  //     console.log("stderr: " + stderr);
+  //   }
+  // );
 
-  workerProcess.on("exit", function (code) {
-    console.log("child_process");
-    console.log("child_process exit ");
-    console.log("child_process exit " + code);
-    console.log("child_process exit ");
-  });
-  console.log(workerProcess.pid);
+  // workerProcess.on("exit", function (code) {
+  //   console.log("child_process");
+  //   console.log("child_process exit ");
+  //   console.log("child_process exit " + code);
+  //   console.log("child_process exit ");
+  // });
 
   getSqlite3().then((database) => {
     console.log("database init suc");
